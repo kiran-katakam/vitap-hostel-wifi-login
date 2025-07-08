5a5ed6b98e725713a7d35d5ab84643f0c2d5f5c6
Dhanumjay Kiran Katakam <katakamkiran53@gmail.com>
2025-07-08 16:18:23 +0530

    Added Wifi Binding Method

--- android/app/src/main/kotlin/com/example/wifi/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wifi/MainActivity.kt
@@ -1,5 +1,46 @@
 package com.example.wifi
 
+import android.net.*
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "wifi.binding/channel"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "bindToWifi") {
+                bindToWifi(result)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun bindToWifi(result: MethodChannel.Result) {
+        val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val builder = NetworkRequest.Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+
+        cm.requestNetwork(builder.build(), object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                val success = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    cm.bindProcessToNetwork(network)
+                } else {
+                    ConnectivityManager.setProcessDefaultNetwork(network)
+                    true
+                }
+                result.success(success)
+            }
+
+            override fun onUnavailable() {
+                result.success(false)
+            }
+        })
+    }
+}
