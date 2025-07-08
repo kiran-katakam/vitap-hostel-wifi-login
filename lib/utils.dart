53ba242ef331ff8a7610e3eb35307a384b5edae9
Dhanumjay Kiran Katakam <katakamkiran53@gmail.com>
2025-07-08 19:22:03 +0530

    Added Save Credentials Function

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
-
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wifi/login.dart';
 
 final String usernameKey = "username";
 final String passwordKey = "password";
+
+final RegExp usernameRegex = RegExp(r'^\d{2}[A-Z]{3}\d{4,5}$');
 
 Future<bool> bindToWifiNetwork() async {
   const platform = MethodChannel('wifi.binding/channel');
@@ -24,7 +26,7 @@ Future<http.Client> createSecureHttpClient() async {
   final context = SecurityContext(withTrustedRoots: true);
 
   final ByteData certData = await rootBundle.load(
-    'assets/cert/ClientAuth_CA.crt',
+    'assets/ClientAuth_CA.crt',
   );
   context.setTrustedCertificatesBytes(certData.buffer.asUint8List());
 
@@ -38,6 +40,7 @@ Future<http.Client> createSecureHttpClient() async {
 }
 
 Future<http.Response> login() async {
+  await bindToWifiNetwork();
   final client = await createSecureHttpClient();
   final SharedPreferences sharedPreferences =
       await SharedPreferences.getInstance();
@@ -76,7 +79,8 @@ Future<http.Response> login() async {
 
 Future<http.Response> logout() async {
   final client = await createSecureHttpClient();
-  final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  final SharedPreferences sharedPreferences =
+      await SharedPreferences.getInstance();
   final String username = sharedPreferences.getString(usernameKey)!;
 
   try {
@@ -106,5 +110,48 @@ Future<http.Response> logout() async {
     throw Exception("❌ Error during logout: $e");
   } finally {
     client.close();
+  }
+}
+
+Future<void> saveCredentials(
+  String username,
+  String password,
+  BuildContext context,
+) async {
+  username = username.toUpperCase().trim();
+  password = password.trim();
+  if (usernameRegex.hasMatch(username) && password.isNotEmpty) {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    final result =
+        await sharedPreferences.setString(usernameKey, username) &&
+        await sharedPreferences.setString(passwordKey, password);
+    if (result) {
+      if (context.mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Login(),)
+        );
+      }
+    } else {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            behavior: SnackBarBehavior.floating,
+            content: Text("Failed to Store the Details"),
+            showCloseIcon: true,
+            duration: Durations.extralong4,
+          ),
+        );
+      }
+    }
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        behavior: SnackBarBehavior.floating,
+        content: Text("Invalid username/password"),
+        showCloseIcon: true,
+        duration: Durations.extralong4,
+      ),
+    );
   }
 }
