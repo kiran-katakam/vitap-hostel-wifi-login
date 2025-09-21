0aa75235d75611e6f0519c7148eaac7185fe0cd6
Dhanumjay Kiran Katakam <katakamkiran53@gmail.com>
2025-09-21 15:42:55 +0530

    Added function to native library to change network connectivity

--- android/app/src/main/kotlin/com/example/wifi/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wifi/MainActivity.kt
@@ -5,19 +5,21 @@ import android.os.Build
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugin.common.MethodChannel
+import android.content.Context
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "wifi.binding/channel"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
 
-        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
             call, result ->
-            if (call.method == "bindToWifi") {
-                bindToWifi(result)
-            } else {
-                result.notImplemented()
+            when (call.method) {
+                "bindToWifi" -> bindToWifi(result)
+                "reportNetworkConnectivity" -> reportNetworkConnectivity(result)
+                else -> result.notImplemented()
             }
         }
     }
@@ -42,5 +44,19 @@ class MainActivity : FlutterActivity() {
                 result.success(false)
             }
         })
+    }
+
+    private fun reportNetworkConnectivity(result: MethodChannel.Result) {
+        try {
+            val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+            val networks = cm.allNetworks
+            for (network in networks) {
+                cm.reportNetworkConnectivity(network, true)
+            }
+            result.success(true)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            result.success(false)
+        }
     }
 }
